e433d1b0d607de0631aea5399d5f3020f5dca4bd
Simone Sestito <simone@simonesestito.com>
2022-12-10 08:49:03 +0100

    Go: better document API handlers registration

--- service/api/api-handler.go
+++ service/api/api-handler.go
@@ -10,6 +10,51 @@ import (
 	"reflect"
 )
 
+// RegisterAll registers all the received controllers, which in turn have a list of single API handlers.
+// The controllers are declared and instantiated in ioc/controller.go.
+//
+// Each feature-specific controller has a `ListRoutes()` method where it declares all its handlers.
+// Then, this function will register them all one by one.
+//
+// By doing so, the controller is not coupled with the registration mechanism and other middlewares.
+// In fact, it's perfectly possible to use another routing method and still no need to touch controllers.
+//
+// For documentation purposes, here they are all:
+// - User related endpoints are registered in features/user/user-controller.go (user.Controller#ListRoutes())
+// -- 'route.SecureRoute' [GET] /users/:userId
+// -- 'route.SecureRoute' [PUT] /users/:userId
+// -- 'route.SecureRoute' [PUT] /users/:userId/username
+// -- 'route.SecureRoute' [GET] /users/
+//
+// - Ban related endpoints are registered in features/user/ban-controller.go (user.BanController#ListRoutes())
+// -- 'route.SecureRoute' [PUT] /users/:userId/bannedPeople/:bannedId
+// -- 'route.SecureRoute' [DELETE] /users/:userId/bannedPeople/:bannedId
+//
+// - Stream related endpoints are registered in features/stream/controller.go (stream.Controller#ListRoutes())
+// -- 'route.SecureRoute' [GET] /users/:userId/stream
+//
+// - Photo related endpoints are registered in features/photo/controller.go (photo.Controller#ListRoutes())
+// -- 'route.SecureRoute' [POST] /photos
+// -- 'route.SecureRoute' [DELETE] /photos/:photoId
+// -- 'route.SecureRoute' [GET] /users/:userId/photos/
+//
+// - Likes related endpoints are registered in features/likes/controller.go (likes.Controller#ListRoutes())
+// -- 'route.SecureRoute' [PUT] /photos/:photoId/likes/:userId
+// -- 'route.SecureRoute' [DELETE] /photos/:photoId/likes/:userId
+//
+// - Follow related endpoints are registered in features/follow/controller.go (follow.Controller#ListRoutes())
+// -- 'route.SecureRoute' [PUT] /users/:userId/followings/:followedId
+// -- 'route.SecureRoute' [DELETE] /users/:userId/followings/:followedId
+// -- 'route.SecureRoute' [GET] /users/:userId/followers/
+// -- 'route.SecureRoute' [GET] /users/:userId/followings/
+//
+// - Comments related endpoints are registered in features/comments/controller.go (comments.Controller#ListRoutes())
+// -- 'route.SecureRoute' [POST] /photos/:photoId/comments/
+// -- 'route.SecureRoute' [DELETE] /photos/:photoId/comments/:commentId
+// -- 'route.SecureRoute' [GET] /photos/:photoId/comments/
+//
+// - Login/auth related endpoints are registered in features/auth/login-controller.go (auth.LoginController#ListRoutes())
+// -- 'route.AnonymousRoute' [POST] /session
 func (router *_router) RegisterAll(controllers []route.Controller) error {
 	// Register routes
 	for _, controller := range controllers {
@@ -23,6 +68,8 @@ func (router *_router) RegisterAll(controllers []route.Controller) error {
 	return nil
 }
 
+// Handler returns the router http.Handler responsible for doing the routing of each request.
+// Registration is performed in the RegisterAll function above.
 func (router *_router) Handler() http.Handler {
 	return router.router
 }
@@ -33,7 +80,9 @@ func (router *_router) RegisterStatic(localPath string, httpPath string) {
 	router.router.ServeFiles(httpPath+"/*filepath", http.Dir(localPath))
 }
 
-// Register a new route
+// Register a new route.
+// Normalize all different kinds of routes. For instance, a route.SecureRoute will
+// be wrapped with a middleware which extracts auth info and performs some checks.
 func (router *_router) Register(routeInfo route.Route) error {
 	// Get route handler
 	var handler route.Handler
@@ -47,7 +96,7 @@ func (router *_router) Register(routeInfo route.Route) error {
 	case isSecure:
 		handler = router.authMiddleware.Intercept(secureRoute.Handler)
 	default:
-		return fmt.Errorf("Unknown route type: %s", reflect.TypeOf(routeInfo))
+		return fmt.Errorf("unknown route type: %s", reflect.TypeOf(routeInfo))
 	}
 
 	// Wrap with other middlewares
